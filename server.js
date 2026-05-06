1edc64a638e74cebc479b2134fad11744384c29c
pbz134 <53271427+pbz134@users.noreply.github.com>
2026-05-06 21:57:50 +0200

    Add playlist-descriptions directory

--- server.js
+++ server.js
@@ -194,6 +194,7 @@ app.use('/subtitles', express.static(path.join(__dirname, 'subtitles')));
 app.use('/livechats', express.static(path.join(__dirname, 'livechats')));
 app.use('/channelposts', express.static(path.join(__dirname, 'channelposts')));
 app.use('/user-profiles', express.static(path.join(__dirname, 'user-profiles')));
+app.use('/playlist-descriptions', express.static(path.join(__dirname, 'playlist-descriptions')));
 app.use('/favicon.png', express.static(path.join(__dirname, 'favicon.png')));
 app.use('/playlist_cache.json', express.static(path.join(__dirname, 'playlist_cache.json')));
 app.use('/video_date_cache.json', express.static(path.join(__dirname, 'video_date_cache.json')));
