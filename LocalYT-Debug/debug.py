14b9d9af4f84a874587acfd7c78f1558ae85acaf
pbz134 <53271427+pbz134@users.noreply.github.com>
2026-06-04 14:07:43 +0200

    Add missing thumbnails-small folder

--- LocalYT-Debug/debug.py
+++ LocalYT-Debug/debug.py
@@ -6,7 +6,7 @@ import signal
 
 # --- PATH CONFIGURATION ---
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CHANNEL_FOLDERS = ['videos', 'subtitles', 'livechats', 'playlist-descriptions', 'channelposts', 'thumbnails', 'videolengths', 'videostats', 'viewcounts', 'descriptions', 'filedates', 'filenames', 'comments', 'channeldesc', 'channelstats', 'channel-home-meta-cache', 'channel-home-previews']
+CHANNEL_FOLDERS = ['videos', 'subtitles', 'livechats', 'playlist-descriptions', 'channelposts', 'thumbnails', 'thumbnails-small', 'videolengths', 'videostats', 'viewcounts', 'descriptions', 'filedates', 'filenames', 'comments', 'channeldesc', 'channelstats', 'channel-home-meta-cache', 'channel-home-previews']
 CHANNEL_FILES = ['channelbanner', 'channelpic', 'subcount']
 MEDIA_EXTENSIONS = ('.mp4', '.mp3', '.mkv')
 JSON_FILES = ['users.json', 'userPreferences.json', 'userCommentLikes.json', 'userSearchHistory.json', 'userSettings.json', 'subscriptions.json', 'watchHistory.json', 'likes.json', 'dislikes.json', 'recommendation_index.json', 'video_cache.json', 'login_attempts.json', 'playlist_cache.json']
